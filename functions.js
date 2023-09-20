33b1800e566fb1993869d6983c7cb7ff51fdbbe6
JaydeepPatidar535 <jaydeepppatidar535@gmail.com>
2023-09-20 13:13:03 +0530

    New File

---
+++ functions.js
@@ -0,0 +1,20 @@
+
+//Factory Function - Create object and return the object but properties are same (camelCase notation)
+
+// function createRectangle(length , breadth){
+//     return rectangle={
+//         length,
+//         breadth,
+
+//         draw:function(){
+//             console.log("Draw a Rectangle")
+//         }
+//     };
+// }
+
+// let rectangle1 = createRectangle(10,20);
+// console.log(rectangle1);
+
+// Constructor function -> pascal notation-> first letter should be capital 
+
+
